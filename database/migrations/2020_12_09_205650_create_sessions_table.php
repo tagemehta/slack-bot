39392162107398d061a9f84e0b30c5c78d3cc082
Tage <tage.mehta@gmail.com>
2020-12-09 16:07:31 -0500

    access_token session

--- database/migrations/2020_12_09_205650_create_sessions_table.php
+++ database/migrations/2020_12_09_205650_create_sessions_table.php
@@ -21,6 +21,7 @@ class CreateSessionsTable extends Migration
             $table->text('payload');
             $table->integer('last_activity')->index();
             $table->text('bot_token');
+            $table->text('access_token');
         });
     }
 
