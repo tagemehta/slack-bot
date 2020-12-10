42a37a488856f239b712676895d80c7c11ac63cd
Tage <tage.mehta@gmail.com>
2020-12-09 19:07:49 -0500

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -42,8 +42,9 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            // 'driver' => 'single',
+            // 'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
