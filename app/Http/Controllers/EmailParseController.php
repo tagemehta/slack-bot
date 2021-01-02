9e456a016155ef95a3d056f8150676ea896d790a
Tage <tage.mehta@gmail.com>
2021-01-01 20:05:37 -0500

    changed parse to work with form

--- app/Http/Controllers/EmailParseController.php
+++ app/Http/Controllers/EmailParseController.php
@@ -9,17 +9,23 @@ class EmailParseController extends Controller
 {
     public function store(Request $request)
     {
-        Log::debug("Hellooooo");
+        
+        $body = $request->input('event.files');
+        Log::debug($body);
         $thread_ts = $request->input('event.ts');
-        $from_email = $request->input('event.files')[0]['from'][0]["address"];
-        $old_email_original_address = $request->input('event.files')[0]['from'][0]["original"];
-        $old_email_body = $request->input('event.files')[0]["plain_text"];
-        $old_email_date_original = $request->input('event.files')[0]["headers"]["date"];
+        if ($body[0]['from'][0]["address"] == "help@classrooms.cloud") {
+            $from_email = $body[0]["headers"]["reply_to"];
+        } else {
+            $from_email = $body[0]['from'][0]["address"];
+        }
+        $old_email_original_address = $from_email;
+        $old_email_body = $body[0]["plain_text"];
+        $old_email_date_original = $body[0]["headers"]["date"];
         $old_email_date_arr = explode(' ', $old_email_date_original);
         $old_email_date = $old_email_date_arr[0] . $old_email_date_arr[2] . " " . $old_email_date_arr[1] . " ". $old_email_date_arr[3] . " " . $old_email_date_arr[4];
-        $email_id = $request->input('event.files')[0]["headers"]["message_id"];
-        $reply_to_address = $request->input('event.files')[0]['to'][0]["address"];
-        $subject = $request->input('event.files')[0]["subject"];
+        $email_id = $body[0]["headers"]["message_id"];
+        $reply_to_address = $body[0]['to'][0]["address"];
+        $subject = $body[0]["subject"];
         if ($subject == null) {
             $subject = "";
         };
