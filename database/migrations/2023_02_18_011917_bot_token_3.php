6e53e19e99df155f673b2fb1c085e8df008998b4
TM64715 <tage.mehta@gmail.com>
2023-02-17 20:20:35 -0500

    migrate botT

--- database/migrations/2023_02_18_011917_bot_token_3.php
+++ database/migrations/2023_02_18_011917_bot_token_3.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class BotToken2023 extends Migration
+class BotToken3 extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class BotToken2023 extends Migration
      */
     public function up()
     {
-        Schema::table('slack_workspaces', function($table) {
-          $table->string('bot_token');
+        Schema::table('slack_workspaces', function(Blueprint $table) {
+          $table->dropColumn('bot_token');
+          $table->renameColumn('user_token', 'bot_token');
       });
     }
 
@@ -25,8 +26,6 @@ class BotToken2023 extends Migration
      */
     public function down()
     {
-        Schema::table('slack_workspaces', function(Blueprint $table) {
-          $table->dropColumn('bot_token');
-        });
+        //
     }
 }
